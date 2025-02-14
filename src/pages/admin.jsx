8bc56c1ff3c4a76d41ee07ce856bda011cc47fca
Brandmerle <brandon.merle@yahoo.com>
2025-02-13 22:49:06 -0500

    109-2

--- src/pages/admin.jsx
+++ src/pages/admin.jsx
@@ -1,6 +1,62 @@
+import { useState } from 'react';
 import "./admin.css"
 
 function Admin() {
+    const [allCoupons, setAllCoupons] = useState([]);
+    const [coupon, setCoupon] = useState({
+        code: "",
+        discount: ""
+    });
+    const [allProducts, setAllProducts] = useState([]);
+    const [product, setProduct] = useState({
+        title:"",
+        price:"",
+        image:"",
+        category:"",
+    });
+
+    function handleProduct(e) {
+        const text = e.target.value;
+        const name = e.target.name;
+        console.log("text changed", text, name);
+
+        let copy ={...product};
+        copy[name]=text;
+        setProduct(copy);
+    }
+
+    function saveProduct(){
+        console.log(product);
+
+        let copy = [...allProducts];
+        copy.push(product);
+        setAllProducts(copy);
+    }
+
+    function handleCoupon(e) {
+        const text = e.target.value;
+        const name = e.target.name;
+        console.log("text changed!", text, name);
+
+        /**
+     * 3 steps to modify Obj or Arrays in State vars:
+     * - Create a copy
+     * - Modify the copy
+     * - Set the copy back
+     */
+    let copy ={...coupon};
+    copy[name]=text;
+    setCoupon(copy);
+    }
+
+    function saveCoupon() {
+        console.log(coupon);
+
+        let copy = [...allCoupons];
+        copy.push(coupon);
+        setAllCoupons(copy);
+    }
+
     return (
         <div className="admin page">
             <h1>Store administration</h1>
@@ -10,23 +66,25 @@ function Admin() {
                     <h5>Manage Products</h5>
                     <div>
                         <label className="form-label">Title</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" onBlur={handleProduct} name="title" />
                     </div>
                     <div>
                         <label className="form-label">Price</label>
-                        <input type="number" className="form-control" />
+                        <input type="number" className="form-control" onBlur={handleProduct} name="price" />
                     </div>
                     <div>
                         <label className="form-label">Image</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" onBlur={handleProduct} name="image"/>
                     </div>
                     <div>
                         <label className="form-label">Category</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control"onBlur={handleProduct} name="category" />
                     </div>
                     <div className="controls">
-                        <button className="btn btn-dark">Save Product</button>
+                        <button className="btn btn-dark" onClick={saveProduct}>Save Product</button>
                     </div>
+                    {allProducts.map(pr => <li>{pr.title} - {pr.category} - ${pr.price}</li>)}
+                    
                 </div>
 
 
@@ -35,17 +93,18 @@ function Admin() {
 
                     <div>
                         <label className="form-label">Code</label>
-                        <input type="text" className="form-control" />
+                        <input type="text" className="form-control" onBlur={handleCoupon} name="code"/>
                     </div>
 
                     <div>
                         <label className="form-label">Discount</label>
-                        <input type="number" className="form-control" />
+                        <input type="number" className="form-control" onBlur={handleCoupon} name="discount"/>
                     </div>
                     <div className="controls">
-                        <button className="btn btn-dark">Save Coupon</button>
+                        <button className="btn btn-dark" onClick={saveCoupon}>Save Coupon</button>
                     </div>
 
+                    {allCoupons.map(cp => <li>{cp.code} - {cp.discount}%</li>)}
                 </div>
             </div>
 
