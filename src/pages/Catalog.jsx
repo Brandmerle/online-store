476455e0e5c6ec1d0ccb7f3035159a52cbde7c93
Brandmerle <brandon.merle@yahoo.com>
2025-02-20 21:33:33 -0500

    109-5

--- src/pages/Catalog.jsx
+++ src/pages/Catalog.jsx
@@ -6,70 +6,70 @@ const catalog = [
   {
     "title": "Blackbird 5ft 4in",
     "image": "/images/5-4.webp",
-    "price": "699.99",
+    "price": 699.99,
     "category": "shortboards",
     "_id": "S001"
   }, 
   {
     "title": "Cordell 5ft 6in",
     "image": "/images/5-6.webp",
-    "price": "659.90",
+    "price": 659.90,
     "category": "shortboards",
     "_id": "S002"
   },
   {
     "title": "Big Kahuna 9ft 6in",
     "image": "images/9-6.webp",
-    "price": "1350.00",
+    "price": 1350.00,
     "category": "longboards",
     "_id": "L001"
   },
   {
     "title": "The Log 9ft 11in",
     "image": "images/9-11.webp",
-    "price": "859.90",
+    "price": 859.90,
     "category": "longboards",
     "_id": "L002"
   },
   {
     "title": "Lost 6ft 4in",
     "image": "images/lost-the-driver6-4.webp",
-    "price": "799.99",
+    "price": 799.99,
     "category": "shortboards",
     "_id": "S003"
   },
   {
     "title": "Modern Blackbird 8ft",
     "image": "images/modern-backbird8.webp",
-    "price": "489.99",
+    "price": 489.99,
     "category": "funboards",
     "_id": "F001"
   },
   {
     "title": "Rat Indust. 6ft 3in",
     "image": "images/rat-wifi-plus6-3.webp",
-    "price": "649.99",
+    "price": 649.99,
     "category": "thrusters",
     "_id": "T001"
   },
   {
     "title": "Richie Collins 6ft 3in",
     "image": "images/richie-collins-twin6-3.webp",
-    "price": "489.99",
+    "price": 489.99,
     "category": "thruster",
     "_id": "T002"
   },
   {
     "title": "Roar Glider 10ft",
     "image": "images/roar-glider10.webp",
-    "price": "999.99",
+    "price": 999.99,
     "category": "longboard",
     "_id": "L003"
   },
   {
     "title": "TJ Pro 9ft",
     "image": "images/Taylor-jensen-tj-pro_9.webp",
-    "price": "889.99",
+    "price": 889.99,
     "category": "longboard",
     "_id": "L004"
   }
@@ -91,7 +91,7 @@ function Catalog() {
       <div className='filters'>
         {categories.map(cat => <button className= "btn btn-small btn-success" >{cat}</button>)}
       </div>
-      {catalog.map(prod => <Product data={prod}/>)}  
+      {catalog.map(prod => <Product key={prod._id} data={prod}/>)}  
       
 
     </div>
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -8,7 +8,7 @@ import DataContext from '../state/dataContext';
 function Cart(){
 
     let cart = useContext(DataContext).cart;
-
+        console.log(cart)
     function calculateNumProds(){
         let sum = 0;
         for(let i=0; i<cart.length; i++){
@@ -18,12 +18,46 @@ function Cart(){
 
         return sum;
     }
+
+    function getTotal(){
+        let total = 0;
+
+        for(let i=0; i<cart.length; i++){
+            const prod = cart[i];
+            total += (prod.quantity * prod.price);
+        }
+
+        return total.toFixed(2);
+    }
+
+
+
     return(
         <div className="cart page">
             <h1>Ready to complete the order?</h1>
 
             <h3>You have {calculateNumProds()} products in your cart</h3>
-            
+
+            <div className="parent">
+                <div className="prods">
+                    {cart.map(prod => <div className="prod-in-cart">
+                        <img src={prod.image} alt="" />
+                        <h5>{prod.title}</h5>
+
+                        <label>${prod.price.toFixed(2)}</label>
+                        <label>{prod.quantity}</label>
+                        <label>${prod.price * prod.quantity}</label>
+                    </div>)}
+                </div>
+
+                <div className="side-menu">
+                    <h3>Order Total</h3>
+                    <h5>${getTotal()}</h5>
+
+                    <button className='btn btn-primary'>Pay now</button>
+                </div>
+
+            </div>
         </div>
     );
 }
